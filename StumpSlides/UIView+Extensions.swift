bb1cc454dc27f2c7d7fb2c1e67f10b63a90fc261
Tom Harrington <tph@atomicbird.com>
2022-08-01 16:28:54 -0600

    Add an optional inset argument

--- StumpSlides/UIView+Extensions.swift
+++ StumpSlides/UIView+Extensions.swift
@@ -9,20 +9,18 @@
 import UIKit
 
 extension UIView {
-    func addSubviewAndConstrain(_ subview: UIView) -> Void {
-        subview.frame = self.bounds
+    func addSubviewAndConstrain(_ subview: UIView, inset: UIEdgeInsets = UIEdgeInsets.zero) -> Void {
+        subview.frame = self.bounds.inset(by: inset)
         subview.translatesAutoresizingMaskIntoConstraints = false
         
         subview.alpha = 1.0
         self.addSubview(subview)
         
         NSLayoutConstraint.activate([
-            self.widthAnchor.constraint(equalTo: subview.widthAnchor, multiplier: 1.0),
-            self.heightAnchor.constraint(equalTo: subview.heightAnchor, multiplier: 1.0),
-            self.leadingAnchor.constraint(equalTo: subview.leadingAnchor),
-            self.trailingAnchor.constraint(equalTo: subview.trailingAnchor),
-            self.topAnchor.constraint(equalTo: subview.topAnchor),
-            self.bottomAnchor.constraint(equalTo: subview.bottomAnchor)
+            self.leadingAnchor.constraint(equalTo: subview.leadingAnchor, constant: inset.left),
+            self.trailingAnchor.constraint(equalTo: subview.trailingAnchor, constant: inset.right),
+            self.topAnchor.constraint(equalTo: subview.topAnchor, constant: inset.top),
+            self.bottomAnchor.constraint(equalTo: subview.bottomAnchor, constant: inset.bottom)
             ])
     }
 }
